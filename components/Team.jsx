e1e455aeae15cf04ad4db19fa753a0120dbb2252
Zane Falcao <112317470+falcaozane@users.noreply.github.com>
2023-07-28 12:32:44 +0530

    images

--- components/Team.jsx
+++ components/Team.jsx
@@ -300,7 +300,11 @@ const Team = () => {
         },
         {
             name: 'Bryce Miranda',
+<<<<<<< Updated upstream
             imgSrc:"/../public/assets/team/Bryce.HEIC",
+=======
+            imgSrc:"/../public/assets/team/Bryce.jpeg",
+>>>>>>> Stashed changes
             position: 'Technical & PR Lead',
             linkedIn:'https://www.linkedin.com/in/bryce-miranda-b7a94a216/',
             Instagram: 'https://instagram.com/thestoryofbryce?igshid=MmIzYWVlNDQ5Yg==',
@@ -328,7 +332,7 @@ const Team = () => {
         },
         {
             name: 'Dhruv Mayekar',
-            imgSrc:"/../public/assets/team/.jpg",
+            imgSrc:"/../public/assets/team/",
             position: 'Graphic Design Co-Lead',
             linkedIn:'https://www.linkedin.com/in/dhruv-mayekar-3a1b35208',
             Instagram: 'https://instagram.com/dhruvv_mayekar?igshid=OGQ5ZDc2ODk2ZA==',
@@ -447,7 +451,7 @@ const Team = () => {
         },
         {
             name: 'Susan Fernandes',
-            imgSrc:"/../public/assets/team/.jpg",
+            imgSrc:"/../public/assets/team/.jpeg",
             position: 'GDSC Associate',
             linkedIn:'https://www.linkedin.com/in/susan-fernandes-3b6635256',
             Instagram: 'https://instagram.com/_susan_fernandes_?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D',
@@ -496,7 +500,7 @@ const Team = () => {
         },
         {
             name: 'Iqra Khan',
-            imgSrc:"/../public/assets/team/.jpg",
+            imgSrc:"/../public/assets/team/",
             position: 'GDSC Associate',
             linkedIn:'https://www.linkedin.com/in/iqrakhan04/',
             Instagram: '',
@@ -523,7 +527,7 @@ const Team = () => {
                                     <div className="flex justify-center -mt-[75px]">
                                         <Image 
                                             src={Member.imgSrc} 
-                                            className="mx-auto rounded-full shadow-lg w-[150px] hover:border-amber-100 hover:border-4" 
+                                            className="mx-auto rounded-full shadow-lg w-[140px] h-[140px] hover:border-amber-100 hover:border-4 " 
                                             alt="Avatar" 
                                             width={100}
                                             height={100}
@@ -546,7 +550,7 @@ const Team = () => {
                                             </svg>
                                             </a>
                                         */}
-                                        <a href="#!" className="px-2 text-slate-400 hover:text-white ">
+                                        <a href={Member.Instagram} className="px-2 text-slate-400 hover:text-white ">
                                             {/* Instagram */}
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
                                             class="h-4 w-4 text-primary ">
@@ -554,7 +558,7 @@ const Team = () => {
                                                 d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                                             </svg>
                                         </a>
-                                        <a href="#!" className="px-2 text-slate-400 hover:text-white ">
+                                        <a href={Member.linkedIn} className="px-2 text-slate-400 hover:text-white ">
                                             {/*Linkedin -->*/}
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
                                             className="h-4 w-4 text-primary ">
