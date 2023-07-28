36e079cd89b82e8cfdc3d6d7f65cc743c18eded7
Zane Fernandes <zane.ferns360@gmail.com>
2023-07-28 16:24:37 +0530

    minor changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -31,7 +31,7 @@ export default function Home() {
       <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
         {/* Your content goes here */}
         <div className="container mx-auto p-8 text-white text-center relative top-[-250px]">
-          <h1 className="text-7xl font-bold blue_gradient max-sm:text-5xl max-sm:pt-28">Welcome to <span className='google_gradient'>GDSC</span></h1>
+          <h1 className="text-7xl font-bold blue_gradient max-sm:text-5xl max-sm:pt-40">Welcome to <span className='google_gradient'>GDSC</span></h1>
           <p className="text-3xl font-bold orange_gradient pt-12">
             Get ready to delve into the World of Developers
           </p>
@@ -42,7 +42,7 @@ export default function Home() {
             </div>
           </div>
           <div className="flex justify-center items-center pt-10">
-      <Link href="/Team1">
+      <Link href="https://gdsc.community.dev/fr-conceicao-rodrigues-college-of-engineering-mumbai/">
         <div className="rounded-full bg-blue-500 text-white py-2 px-4 cursor-pointer text-xl font-bold transform transition-all hover:scale-110 shadow-lg hover:shadow-xl">
           Join Us
         </div>
