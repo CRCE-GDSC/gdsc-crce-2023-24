92c1de9bc578d1e4d5870bd0b0b1445c824ee2a3
Zane Fernandes <zane.ferns360@gmail.com>
2023-07-28 18:05:31 +0530

    responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -30,7 +30,7 @@ export default function Home() {
       {/* Content Container */}
       <div className="absolute inset-0 flex flex-col items-center justify-center z-10 ">
         {/* Your content goes here */}
-        <div className="container mx-auto p-8 text-white text-center relative top-[-250px] mt-48">
+        <div className="container mx-auto p-8 text-white text-center relative top-[-250px] mt-48 pt-40">
           <h1 className="text-7xl font-bold blue_gradient max-sm:text-5xl max-sm:pt-40 text-left">Welcome to <span className='google_gradient'>GDSC</span></h1>
           <p className="text-3xl font-bold orange_gradient pt-12 text-left">
             Get ready to delve into the World of Developers
