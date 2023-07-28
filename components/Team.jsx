9a712c74361e8d3a68ab0a66c752a7bf0cc0d014
Zane Falcao <112317470+falcaozane@users.noreply.github.com>
2023-07-28 16:42:04 +0530

    team updated

--- components/Team.jsx
+++ components/Team.jsx
@@ -525,12 +525,12 @@ const Team = () => {
                             <div key={index} className="mb-24 md:mb-0 py-24">
                             <div
                                 
-                                className="block h-full rounded-lg bg-blue-600 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] hover:bg-blue-700">
+                                className="block h-full rounded-lg bg-amber-50 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] hover:bg-orange-50 hover:translate-y-5">
                                 <div className="flex justify-center">
                                     <div className="flex justify-center -mt-[75px]">
                                         <Image 
                                             src={Member.imgSrc} 
-                                            className="mx-auto rounded-full shadow-lg w-[140px] h-[140px] hover:border-amber-100 hover:border-4" 
+                                            className="mx-auto rounded-full shadow-lg w-[140px] h-[140px] hover:border-blue-500 hover:border-4 " 
                                             alt="Avatar" 
                                             
                                             width={500}
@@ -542,8 +542,8 @@ const Team = () => {
                                     </div>
                                 </div>
                                 <div className="p-6">
-                                    <h5 className="mb-4 text-xl font-extrabold text-white">{Member.name}</h5>
-                                    <p className="mb-6 font-semibold text-slate-300">{Member.position}</p>
+                                    <h5 className="mb-4 text-xl font-extrabold text-gray-700">{Member.name}</h5>
+                                    <p className="mb-6 font-semibold text-slate-500">{Member.position}</p>
                                     { (Member.linkedIn || Member.Instagram) ?(
                                         <ul className="mx-auto flex list-inside justify-center">
                                         {/*
@@ -556,7 +556,7 @@ const Team = () => {
                                             </svg>
                                             </a>
                                         */}
-                                        <a href={Member.Instagram} className="px-2 text-slate-400 hover:text-white ">
+                                        <a href={Member.Instagram} className="p-4 rounded-full text-slate-500 hover:bg-blue-300 hover:text-white">
                                             {/* Instagram */}
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
                                             class="h-4 w-4 text-primary ">
@@ -564,7 +564,7 @@ const Team = () => {
                                                 d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                                             </svg>
                                         </a>
-                                        <a href={Member.linkedIn} className="px-2 text-slate-400 hover:text-white ">
+                                        <a href={Member.linkedIn} className="p-4 rounded-full text-slate-500 hover:bg-blue-300 hover:text-white">
                                             {/*Linkedin -->*/}
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
                                             className="h-4 w-4 text-primary ">
@@ -586,7 +586,7 @@ const Team = () => {
                             <div key={index} className="mb-24 md:mb-0 py-24">
                             <div
                                 
-                                className="block h-full rounded-lg bg-blue-600 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] hover:bg-blue-700">
+                                className="block h-full rounded-lg bg-amber-50 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] hover:bg-blue-700">
                                 <div className="flex justify-center">
                                     <div className="flex justify-center -mt-[75px]">
                                         <Image 
