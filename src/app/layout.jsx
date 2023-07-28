1142ce1af650ef471b2c4b88f1aa3c36eef71970
Rudalph <gonsalvesrudalph@gmail.com>
2023-07-28 09:36:22 +0530

    What we do

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,11 +2,7 @@ import 'style/global.css';
 import Nav from 'components/Nav';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google'
-<<<<<<< HEAD
 import Whatwedo from '../../components/ui/Whatwedo';
-=======
-import Team from '../../components/Team';
->>>>>>> c246008b636df7f4a72c3ee2bcdf2735e592f909
 
 export const metadata ={
   title:"GDSC CRCE",
@@ -25,12 +21,8 @@ const RootLayout = ({children}) => {
               <Nav/>
                {children}
                <Analytics />
-<<<<<<< HEAD
-               <Whatwedo/>
+      
                 </main>
-=======
-            </main>
->>>>>>> c246008b636df7f4a72c3ee2bcdf2735e592f909
         </body>
         </html>
   )
