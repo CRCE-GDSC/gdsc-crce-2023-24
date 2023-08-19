015ab793f08c966ed04ced6e2c79e9c33609ec6a
AlvinDHacker <122222739+AlvinDHacker@users.noreply.github.com>
2023-08-19 13:37:36 +0530

    Made Edits on Profile

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div className='mt-16 pt-10'>
-      
-    <div class="w-full max-w-sm bg-white rounded-lg dark:bg-gray-800">
-    {/* <div class="flex justify-end px-4 pt-4">
+    <div className="mt-16 pt-10">
+      <div class="w-full mx-auto max-w-sm bg-white rounded-lg">
+        {/* <div class="flex justify-end px-4 pt-4">
         <button id="dropdownButton" data-dropdown-toggle="dropdown" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
             <span class="sr-only">Open dropdown</span>
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
@@ -27,14 +26,106 @@ const Profile = () => {
             </ul>
         </div>
     </div> */}
-    <div class="flex flex-col items-center pb-10">
-        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/assets/team/alvin.jpg" alt="User image"/>
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">User Name</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400 uppercase">User Class</span>
-        <span class="text-sm text-gray-500 dark:text-gray-400">User email</span>
-    </div>
+        <div class="flex flex-col items-center pb-10">
+          <img
+            class="w-24 h-24 mb-3 rounded-full shadow-lg"
+            src="/assets/team/alvin.jpg"
+            alt="User image"
+          />
+          <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+            User Name
+          </h5>
+          <span class="text-sm text-[#F44336] dark:text-gray-400 uppercase ">
+            User Class
+          </span>
+          <span class="text-sm text-gray-500 dark:text-gray-400">
+            User email
+          </span>
+        </div>
+      </div>
+
+      <div className="grid md:grid-cols-3 gap-3">
+        <a
+          href="#"
+          class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        > 
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p class="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </a>
+
+        <a
+          href="#"
+          class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p class="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </a>
+
+        <a
+          href="#"
+          class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p class="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </a>
+      </div>
+
+      <div className='mt-5 mx-5'>
+        <h1 className='text-lg font-semibold '>Events</h1>
+        
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Event name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Preview
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Status
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Points
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Web Dev Workshop
+                </th>
+                <td class="px-6 py-4">
+                    <img src="" alt="" />
+                </td>
+                <td class="px-6 py-4">
+                    Attended
+                </td>
+                <td class="px-6 py-4">
+                    50
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 
+      </div>
     </div>
   )
 }
