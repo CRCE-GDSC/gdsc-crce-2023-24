8ecf584023a6811d4faab9dad58ddd916a28389a
Zane Fernandes <zane.ferns360@gmail.com>
2023-08-20 20:18:05 +0530

    student data ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -26,4 +27,4 @@ module.exports = {
     themes: ["light"],
   },
 
-}
+});
