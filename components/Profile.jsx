7d38ad9372f777a3ba0d57837657847b3e218b6f
Chhand Chaughule <chhandchaughule123@gmail.com>
2023-08-22 15:38:56 +0530

    qr code error fixed

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -35,16 +35,7 @@ const Profile = ({ userDisplayName, paramsUserName, userProfilePic, userEmail, u
     
   ]
   
-  const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${userUID}`; // Replace with the actual API URL
-  const imageElement = document.getElementById('img-2'); // Replace with your HTML element ID
   
-  getImageFromAPI(apiUrl)
-  .then(imageURL => {
-    if (imageURL) {
-      // Set the image source
-      imageElement.src = imageURL;
-    }
-  });
 
   const events = [
     {
@@ -60,7 +51,6 @@ const Profile = ({ userDisplayName, paramsUserName, userProfilePic, userEmail, u
       eventTags: '25',
     },
   ]
-
 
 
   
@@ -93,20 +83,48 @@ const Profile = ({ userDisplayName, paramsUserName, userProfilePic, userEmail, u
   };
   return (
     <div>
-      <div class="w-full mx-auto bg-white rounded-lg">
+      <div className="w-full mx-auto bg-white rounded-lg">
         {user.map((values, index) => (
-          <div key={index} class="flex flex-col items-center pb-10">
-            <div id='img-cont' class="image-container">
-            <img src={userProfilePic} class='img-1' id='img-1'/>
-            <img src="https://images.unsplash.com/photo-1682687982167-d7fb3ed8541d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=600&q=60" class='img-2' id='img-2'/>
+          <div key={index} className="flex flex-col items-center pb-10">
+            <div id='img-cont' className="image-container">
+            <img src={userProfilePic} className='img-1' id='img-1' onClick={()=>
+            {
+              const container = document.getElementById('img-cont');
+              const img_1 = document.getElementById('img-1');
+              const img_2 = document.getElementById('img-2');
+              img_1.style.transform = 'scale(0)';
+              img_2.style.transform = 'scale(1)';
+              img_2.style.opacity = '1';
+              img_1.style.opacity = '0';
+              container.style.borderRadius = '0';
+              img_2.style.borderRadius = '0';}}/>
+            <img src="https://images.unsplash.com/photo-1682687982167-d7fb3ed8541d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=600&q=60" className='img-2' id='img-2' onLoad={()=>{
+              const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${userUID}`; // Replace with the actual API URL
+              const imageElement = document.getElementById('img-2');
+
+              getImageFromAPI(apiUrl)
+              .then(imageURL => {
+                if (imageURL) {
+                  // Set the image source
+                  imageElement.src = imageURL;
+                }
+              });
+    }} onClick={()=>{
+              const img_1 = document.getElementById('img-1');
+              const img_2 = document.getElementById('img-2');
+              img_2.style.transform = 'scale(0)';
+              img_1.style.transform = 'scale(1)';
+              img_1.style.opacity = '1';
+              img_2.style.opacity = '0';
+            }} />
             </div>
-            <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+            <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
               {userDisplayName}
             </h5>
-            <span class="text-sm text-[#F44336] dark:text-gray-400 uppercase ">
+            <span className="text-sm text-[#F44336] dark:text-gray-400 uppercase ">
               {values.userclass}
             </span>
-            <span class="text-sm text-gray-500 dark:text-gray-400">
+            <span className="text-sm text-gray-500 dark:text-gray-400">
               {userEmail}
             </span>
           </div>
@@ -115,7 +133,7 @@ const Profile = ({ userDisplayName, paramsUserName, userProfilePic, userEmail, u
       <div className='w-[75%] mx-auto'>
         <div className="grid md:grid-cols-2 gap-8 mt-6 justify-center">
           <div className="relative overflow-hidden x-shadow-yellow feature-card p-5 h-[150px] max-w-[18rem]">
-            <h5 class="font-inter font-bold mb-2 text-3xl tracking-tight text-[#FFCA28] dark:text-white">
+            <h5 className="font-inter font-bold mb-2 text-3xl tracking-tight text-[#FFCA28] dark:text-white">
               YOUR TAGS
             </h5>
 
@@ -134,11 +152,11 @@ const Profile = ({ userDisplayName, paramsUserName, userProfilePic, userEmail, u
           </div>
 
           <div className="relative overflow-hidden x-shadow-red feature-card p-5 h-[150px] max-w-[18rem] ml-auto">
-  <h5 class="font-inter font-bold mb-2 text-3xl tracking-tight text-[#F44336] dark:text-white">
+  <h5 className="font-inter font-bold mb-2 text-3xl tracking-tight text-[#F44336] dark:text-white">
     LEADERBOARD
   </h5>
   {user.map((values, index) => (
-    <div key={index} class="font-inter text-3xl text-black dark:text-gray-400">
+    <div key={index} className="font-inter text-3xl text-black dark:text-gray-400">
       <div className="flex justify-between items-center">
         <div>
           <p className='font-semibold'>Rank: {values.userranking}</p>
@@ -161,31 +179,31 @@ const Profile = ({ userDisplayName, paramsUserName, userProfilePic, userEmail, u
       <div className="my-10 mx-7">
         <h1 className="text-2xl font-semibold ">Event Status</h1>
         <div className="mt-4 x-shadow-blue">
-         <div class="relative overflow-x-auto rounded-xl">
-  <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 rounded-xl border-2 overflow-hidden">
-    <thead class="text-xl text-[#FFCA28] uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+         <div className="relative overflow-x-auto rounded-xl">
+  <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 rounded-xl border-2 overflow-hidden">
+    <thead className="text-xl text-[#FFCA28] uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
       <tr>
         <th
           scope="col"
-          class="px-4 md:px-6 py-3 border-b dark:border-gray-700 md:text-lg border-r border-black"
+          className="px-4 md:px-6 py-3 border-b dark:border-gray-700 md:text-lg border-r border-black"
         >
           Event name
         </th>
         <th
           scope="col"
-          class="px-4 md:px-6 py-3 border-b dark:border-gray-700 md:text-lg flex justify-center items-center border-r border-black"
+          className="px-4 md:px-6 py-3 border-b dark:border-gray-700 md:text-lg flex justify-center items-center border-r border-black"
         >
           Preview
         </th>
         <th
           scope="col"
-          class="px-4 md:px-6 py-3 border-b text-center border-r text-lg border-black"
+          className="px-4 md:px-6 py-3 border-b text-center border-r text-lg border-black"
         >
           Status
         </th>
         <th
           scope="col"
-          class="px-4 md:px-6 py-3 border-b text-center text-lg border-black"
+          className="px-4 md:px-6 py-3 border-b text-center text-lg border-black"
         >
           Tags
         </th>
@@ -195,15 +213,15 @@ const Profile = ({ userDisplayName, paramsUserName, userProfilePic, userEmail, u
       {events.map((values, index) => (
         <tr
           key={index}
-          class="bg-white border-b border-black dark:bg-gray-800 dark:border-gray-700"
+          className="bg-white border-b border-black dark:bg-gray-800 dark:border-gray-700"
         >
           <td
             scope="row"
-            class="px-4 md:px-6 py-4 font-medium border-black text-[#4285F4] whitespace-nowrap dark:text-white md:text-lg border-r"
+            className="px-4 md:px-6 py-4 font-medium border-black text-[#4285F4] whitespace-nowrap dark:text-white md:text-lg border-r"
           >
             {values.eventname}
           </td>
-          <td class="px-4 md:px-6 py-4 border-black dark:border-gray-700 text-lg md:flex md:justify-center md:items-center border-r">
+          <td className="px-4 md:px-6 py-4 border-black dark:border-gray-700 text-lg md:flex md:justify-center md:items-center border-r">
             <Image
               src={values.eventpreview}
               alt=""
@@ -221,7 +239,7 @@ const Profile = ({ userDisplayName, paramsUserName, userProfilePic, userEmail, u
           >
             {values.eventstatus}
           </td>
-          <td class="px-4 md:px-6 py-4 border-b text-center border-black dark:border-gray-700 text-[#35A853] text-lg">
+          <td className="px-4 md:px-6 py-4 border-b text-center border-black dark:border-gray-700 text-[#35A853] text-lg">
             {values.eventTags}
           </td>
         </tr>
@@ -237,7 +255,7 @@ const Profile = ({ userDisplayName, paramsUserName, userProfilePic, userEmail, u
      
 
         {/* <div className="relative overflow-hidden x-shadow-red feature-card p-5 h-[150px] max-w-[18rem]">
-  <h5 class="font-inter font-bold mb-2 text-3xl tracking-tight text-[#F44336] dark:text-white">
+  <h5 className="font-inter font-bold mb-2 text-3xl tracking-tight text-[#F44336] dark:text-white">
     LEADER BOARD
   </h5>
   {/* This is a comment-> Loop through the top 10 users */}
@@ -245,7 +263,7 @@ const Profile = ({ userDisplayName, paramsUserName, userProfilePic, userEmail, u
   {/* {user.slice(0, 10).map((values, index) => (
     <div
       key={index}
-      class="font-inter text-3xl text-black dark:text-gray-400"
+      className="font-inter text-3xl text-black dark:text-gray-400"
     >
       <p>Rank: {index + 1}</p>
       <p>{values.username}</p>
@@ -358,24 +376,38 @@ const Profile = ({ userDisplayName, paramsUserName, userProfilePic, userEmail, u
   )
 }
 
-const container = document.getElementById('img-cont');
-const img_1 = document.getElementById('img-1');
-const img_2 = document.getElementById('img-2');
+   // Replace with your HTML element ID
+// window.onload = function(){  
+//   // getImageFromAPI(apiUrl)
+//   // .then(imageURL => {
+//   //   if (imageURL) {
+//   //     // Set the image source
+//   //     imageElement.src = imageURL;
+//   //   }
+//   // });
+  
+// }
 
-img_1.onclick = function () {
-  img_1.style.transform = 'scale(0)';
-  img_2.style.transform = 'scale(1)';
-  img_2.style.opacity = '1';
-  img_1.style.opacity = '0';
-  container.style.borderRadius = '0';
-  img_2.style.borderRadius = '0';
-};
+// const container = document.getElementById('img-cont');
+// const img_1 = document.getElementById('img-1');
+// const img_2 = document.getElementById('img-2');
 
-img_2.onclick = function () {
-  img_2.style.transform = 'scale(0)';
-  img_1.style.transform = 'scale(1)';
-  img_1.style.opacity = '1';
-  img_2.style.opacity = '0';
-};
+// console.log(img_1);
+// console.log(img_2);
 
+// img_1.onclick = function () {
+//   // img_1.style.transform = 'scale(0)';
+//   // img_2.style.transform = 'scale(1)';
+//   // img_2.style.opacity = '1';
+//   // img_1.style.opacity = '0';
+//   // container.style.borderRadius = '0';
+//   // img_2.style.borderRadius = '0';
+// };
+
+// img_2.onclick = function () {
+//   // img_2.style.transform = 'scale(0)';
+//   // img_1.style.transform = 'scale(1)';
+//   // img_1.style.opacity = '1';
+//   // img_2.style.opacity = '0';
+// };
 export default Profile
